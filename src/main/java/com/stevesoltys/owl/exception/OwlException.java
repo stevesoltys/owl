9b7556bf0cc04cb091f4bdcc81c4e3a367267037
Steve Soltys <stevesoltys@live.com>
2016-03-13 19:57:25 -0400

    Add OwlException

---
+++ src/main/java/com/stevesoltys/owl/exception/OwlException.java
@@ -0,0 +1,11 @@
+package com.stevesoltys.owl.exception;
+
+/**
+ * @author Steve Soltys
+ */
+public class OwlException extends Exception {
+
+    public OwlException(String message) {
+        super(message);
+    }
+}
