ba94fc8bc31490555131bedc67ccd6618f5cd408
Steve Soltys <stevesoltys@live.com>
2016-03-13 19:46:53 -0400

    Remove unused imports from Owl.java

--- src/main/java/com/stevesoltys/owl/Owl.java
+++ src/main/java/com/stevesoltys/owl/Owl.java
@@ -1,7 +1,5 @@
 package com.stevesoltys.owl;
 
-import com.stevesoltys.owl.config.OwlConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
