b8841ae0ea83580f2b4d6c87e3b8c98ccfeda41e
Steve Soltys <stevesoltys@live.com>
2016-03-16 16:06:35 -0400

    Update OwlConfigurationTests, now mocks autowired classes

--- src/test/java/com/stevesoltys/owl/config/OwlConfigurationTests.java
+++ src/test/java/com/stevesoltys/owl/config/OwlConfigurationTests.java
@@ -1,39 +1,59 @@
 package com.stevesoltys.owl.config;
 
-import com.stevesoltys.owl.exception.OwlConfigurationException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author Steve Soltys
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration()
 public class OwlConfigurationTests {
 
+    @Autowired
+    private OwlConfiguration owlConfiguration;
+
     /**
-     * Tests failing of {@link OwlAgentConfiguration#initialize(Map)} when the given configuration is invalid.
+     * Tests failing of {@link OwlComponentTypeConfiguration#initialize(Map)} when the given configuration is invalid.
      */
     @Test(expected = Exception.class)
-    public void testInvalidAgentConfiguration() throws Exception {
+    public void testInvalidComponentTypeConfiguration() throws Exception {
         Map<String, Object> configuration = new HashMap<>();
         configuration.put("wrong", "config");
 
-        OwlAgentConfiguration agentConfiguration = new OwlAgentConfiguration();
-        agentConfiguration.initialize(configuration);
+        OwlComponentTypeConfiguration componentTypeConfiguration = new OwlComponentTypeConfiguration();
+        componentTypeConfiguration.initialize(configuration);
     }
 
     /**
-     * Tests {@link OwlAgentConfiguration#initialize(Map)} when the configuration is valid.
+     * Tests {@link OwlConfiguration#initialize(Map)} when the configuration is valid.
      */
     @Test
-    public void testValidAgentConfiguration() throws Exception {
+    public void testValidComponentTypeConfiguration() throws Exception {
+
         Map<String, Object> configuration = new HashMap<>();
+
         configuration.put("agent", true);
 
-        OwlAgentConfiguration agentConfiguration = new OwlAgentConfiguration();
-        agentConfiguration.initialize(configuration);
+        List<Map<String, String>> componentTypeList = new LinkedList<>();
+
+        Map<String, String> componentType = new HashMap<>();
+        componentType.put("identifier", "cpu_load");
+        componentType.put("classpath", "com.stevesoltys.owl.model.component.CPULoadComponent");
+        componentType.put("controller_classpath", "com.stevesoltys.owl.controller.component.CPULoadComponentController");
+
+        componentTypeList.add(componentType);
+        configuration.put("component_types", componentTypeList);
+
+        owlConfiguration.initialize(configuration);
     }
 
 }
